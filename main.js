a7b67414c4b9f3597bb0ef9718c56a65da795647
prince kashyap <vasukashyap08@gmail.com>
2023-05-04 15:41:53 +0530

    commit

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 // Current-year 
 const year = document.querySelector('#current-year')
 year.innerHTML = new Date().getFullYear();
+
+//  owlcarousal 
+$(".carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 1,
+            nav: false
+        },
+        1000: {
+            items: 1,
+            nav: false
+        },
+    }
+});
