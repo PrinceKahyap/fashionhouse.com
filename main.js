c7e0f226d6829372918f72160f5b99c77c508f99
prince kashyap <vasukashyap08@gmail.com>
2023-06-08 13:32:08 +0530

    commit

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 // Hamburger 
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-// hamburger.addEventListener("click",() => {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click",() => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
 
 // footer date 
 const year = document.querySelector(".current-year")
 year.innerHTML = new Date().getFullYear();
+
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+};
