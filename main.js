7669b3a7610e715ee6233b5e490f1a167f7737ef
prince kashyap <vasukashyap08@gmail.com>
2023-06-15 19:39:11 +0530

    commit

--- main.js
+++ main.js
@@ -5,4 +5,18 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
+
+// Search-box 
+function openSearch(){
+    document.getElementById("navOverlay").style.display = "block";
+}
+function closeSearch(){
+    document.getElementById("navOverlay").style.display = "none";
+};
+
+const preloader = document.getElementById("loader");
+function myfunction(){
+    preloader.style.display = 'none';
+
+}
